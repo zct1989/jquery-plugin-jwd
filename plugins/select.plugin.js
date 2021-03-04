836837c5f91296217b4f02af0e8c4cab50e038ea
zhuchentong <2037630@163.com>
2021-03-04 11:12:45 +0800

    update

--- plugins/select.plugin.js
+++ plugins/select.plugin.js
@@ -24,16 +24,14 @@ const styles = `
    font-weight: bold;
  }
 
-
  .data-group{
     display:flex;
     align-items:center;
     margin:5px;
     border-bottom:solid 1px rgba(0,0,0,0.1);
     padding:10px;
-
  }
-
+ 
  .data-group .data-group-title{
   min-width:80px;
   letter-spacing:1em;
@@ -65,6 +63,52 @@ const styles = `
     color:#fff;
     border-radius:40px;
  }
+
+ .current-group{
+    display:flex;
+    align-items:center;
+    margin:5px;
+    border-bottom:solid 1px rgba(0,0,0,0.1);
+    padding:10px;
+ }
+ 
+ .current-group .group-title{
+  min-width:80px;
+  font-weight:bold;
+  color:#AFAFAF;
+  font-size:14px;
+ }
+
+ .current-group .group-title::after{
+    content:':';
+    padding-left:1em;
+ }
+
+ .current-group .group-container{
+    display:flex;
+ }
+
+ .current-group .current-group-item{
+    padding: 5px 10px;
+    padding-right:15px;
+    margin: 0 10px;
+    font-size: 14px;
+    font-weight: bold;
+    min-width:60px;
+    text-align:center;
+    border-radius:50px;
+    border:solid 1px #0079FE;
+    color:#0079FE;
+    position:relative;
+ }
+
+ .current-group .current-group-item .current-group-close{
+    cursor:pointer;
+    position:absolute;
+    right:4px;
+    top:-1px;
+    padding:5px;
+  }
 `;
 
 function SelectArea(data, onChange) {
@@ -73,6 +117,7 @@ function SelectArea(data, onChange) {
   this.$level = 0;
   this.$onChange = function () {
     onChange && onChange(this.$value);
+    this.updateCurrentGroup();
   };
   this.$groupContainer = $(`<div class="group-container"></div>`);
 }
@@ -83,14 +128,64 @@ SelectArea.prototype.render = function () {
 
   const roots = this.$data.filter((x) => !x.parent);
   const rootGroup = this.generateRootGroup();
+  const currentGroup = this.generateCurrentGroup();
 
   this.$element.push(rootGroup);
   this.$element.push(this.$groupContainer);
+  this.$element.push(currentGroup);
 
   const root = roots[0];
   this.generate(root, 0);
 
+  setTimeout(() => {
+    this.updateCurrentGroup();
+  });
+
   return this.$element;
+};
+
+SelectArea.prototype.generateCurrentGroup = function () {
+  const group = $('<div class="current-group"></div>');
+  const groupTitle = $("<div class='group-title'></div>");
+  const groupContainer = $("<div class='group-container'></div>");
+
+  groupTitle.text("已选位置");
+
+  group.append(groupTitle, groupContainer);
+  return group;
+};
+
+SelectArea.prototype.updateCurrentGroup = function () {
+  const groupContainer = $(".current-group .group-container");
+  groupContainer.empty();
+  const groups = this.$value.map((item, index) => {
+    const node = $(`<div class="current-group-item"></div>`);
+    const text = $(`<div class="current-group-text" ></div>`);
+    const close = $(`<div class="current-group-close"></div>`);
+
+    close.click(() => {
+      const level = index;
+      const prev = $(`.data-group-level-${level} .data-group-item.active`);
+      const node = $(
+        `.data-group-level-${level} .data-group-item.all-selection`
+      );
+      prev.removeClass("active");
+      node.addClass("active");
+
+      this.generate(null, level);
+      this.$value = this.$value.slice(0, level);
+      this.$onChange();
+    });
+
+    node.append(text);
+    if (index !== 0) node.append(close);
+
+    text.text(item.title);
+    close.text("x");
+    return node;
+  });
+
+  groupContainer.append(groups);
 };
 
 SelectArea.prototype.generate = function (data, _level) {
@@ -189,7 +284,7 @@ SelectArea.prototype.generateGroup = function (list, level) {
 };
 
 SelectArea.prototype.generateAllSelectGroupItem = function (level) {
-  const node = $("<div class='data-group-item'></div>");
+  const node = $("<div class='data-group-item all-selection'></div>");
 
   // 设置文本
   node.text("全选");
